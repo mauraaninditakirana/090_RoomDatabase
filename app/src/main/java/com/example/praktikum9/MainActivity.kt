be3339adc2dbed9dd28b2aabf20253a0ea6d4da5
mauraaninditakirana <kiranamaura351@gmail.com>
2025-11-27 14:31:10 +0700

    menampilkan SiswaApp di MainActivity

--- app/src/main/java/com/example/praktikum9/MainActivity.kt
+++ app/src/main/java/com/example/praktikum9/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.praktikum9.ui.theme.Praktikum9Theme
+import com.example.praktikum9.view.uicontroller.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
