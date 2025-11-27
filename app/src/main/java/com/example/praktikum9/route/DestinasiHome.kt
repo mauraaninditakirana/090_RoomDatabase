da44c8f0d96d881559e9019e4eab98d4273e911e
mauraaninditakirana <kiranamaura351@gmail.com>
2025-11-27 13:29:00 +0700

    membuat route DestinasiHome

---
+++ app/src/main/java/com/example/praktikum9/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.praktikum9.route
+
+import com.example.praktikum9.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
