3d8f4ab8818d7b63a6f69472621b07ec4e678a13
mauraaninditakirana <kiranamaura351@gmail.com>
2025-11-27 13:31:41 +0700

    membuat route DestinasiEntry

---
+++ app/src/main/java/com/example/praktikum9/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.praktikum9.route
+
+import com.example.praktikum9.R
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+
+}
