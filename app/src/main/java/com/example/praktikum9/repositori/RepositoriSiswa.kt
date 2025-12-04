d9b9ada17d833625072112efd778d47ab011c058
mauraaninditakirana <kiranamaura351@gmail.com>
2025-12-04 11:39:07 +0700

    Menambahkan fungsi getSiswaStream dan deleteSiswa di RepositoriSiswa.kt

--- app/src/main/java/com/example/praktikum9/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/praktikum9/repositori/RepositoriSiswa.kt
@@ -8,6 +8,10 @@ interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
 
     suspend fun insertSiswa(siswa: Siswa)
+
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+    suspend fun deleteSiswa(siswa: Siswa)
+
 }
 
 class OfflineRepositoriSiswa(
@@ -17,4 +21,9 @@ class OfflineRepositoriSiswa(
         .getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao
         .insert(siswa)
+
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
 }
+
+
