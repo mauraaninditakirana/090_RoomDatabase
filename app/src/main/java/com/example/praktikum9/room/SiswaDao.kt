a6f040ab78b130a0f206e013457187fba6d477fe
mauraaninditakirana <kiranamaura351@gmail.com>
2025-12-04 11:29:05 +0700

    menambahkan getSiswa sesuai dengan id di DAO

--- app/src/main/java/com/example/praktikum9/room/SiswaDao.kt
+++ app/src/main/java/com/example/praktikum9/room/SiswaDao.kt
@@ -13,4 +13,7 @@ interface SiswaDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
+
+    @Query("SELECT * from tblsiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa>
 }
